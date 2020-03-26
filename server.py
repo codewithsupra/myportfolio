2b120494bea4174e80e9faf46c5c18c9608eba22
SUPRATIM <supratim.cfc@outlook.com>
2020-03-26 20:02:15 +0400

    updates

---
+++ server.py
@@ -0,0 +1,19 @@
+from flask import Flask,render_template,url_for,request,redirect
+import csv
+app = Flask(__name__)
+@app.route('/index.html')
+def homepage():
+    return render_template('index.html' )
+
+@app.route('/<string:page_name>')
+def workpage(page_name):
+    return render_template(page_name )
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+	if request.method=='POST':
+	 data=request.form.to_dict()
+	 print(data)
+	 return redirect('/thankyou.html')
+	else:
+	 return'something went wrong! please try again' 
+  
